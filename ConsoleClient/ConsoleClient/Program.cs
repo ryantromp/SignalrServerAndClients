011f6e65585bee42e889928353eb4ac3594a40c1
Ryan Tromp <ryan.tromp@gmail.com>
2018-04-26 14:07:11 +0100

    Initial commit.

---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            try
+            {
+                const string connectionUrl = "http://localhost:9000/eventHub";
+
+                var hubConnection = new HubConnectionBuilder()
+                    .WithUrl(connectionUrl)
+                    .WithConsoleLogger(LogLevel.Trace)
+                    .Build();
+
+                await hubConnection.StartAsync();
+                
+                Console.WriteLine("Connected to signalR server...");
+                Console.ReadKey();
+            }
+            catch (HubException exception)
+            {
+                Console.WriteLine(exception);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Server/Server/EventHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server
+{
+    public class EventHub : Hub
+    {        
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Client connected");
+            return base.OnConnectedAsync();
+        }
+    }
+}
